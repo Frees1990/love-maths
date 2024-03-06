4c56a00588efc8cf71761930d90b81a9e4a428e6
Frees1990 <fairmejor@gmail.com>
2024-03-06 07:22:10 +0000

    Set up structure of game functions

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivisionQuestion(){
+    
+}
